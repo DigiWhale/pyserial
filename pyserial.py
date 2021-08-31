c3584e352a6f4df7624bcaac02b6d0331c796adb
digiwhale <digiwhale.humphrey@gmail.com>
2021-08-30 22:32:16 -0400

    3

--- pyserial.py
+++ pyserial.py
@@ -1,6 +1,6 @@
 import serial
 
 ser=serial.Serial('/dev/serial0',115200, timeout = 10)
-readedText = ser.read(100).decode('utf-8')
+readedText = ser.read(100).decode("ascii")
 print(readedText)
 ser.close()
