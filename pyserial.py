bb195d61ea79cb26ce341255e154a4cf747dce24
digiwhale <digiwhale.humphrey@gmail.com>
2021-08-30 22:22:34 -0400

    5

--- pyserial.py
+++ pyserial.py
@@ -1,6 +1,6 @@
 import serial
 
-ser=serial.Serial('/dev/ttyAMA0',115200, timeout = 10)
+ser=serial.Serial('/dev/ttyUSB0',115200, timeout = 10)
 readedText = ser.read(ser.inWaiting())
 print(readedText)
 ser.close()
